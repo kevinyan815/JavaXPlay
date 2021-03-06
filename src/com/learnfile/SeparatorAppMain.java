8d032b92df970c2dcbd8339e09491a4bcfeb398f
kevinyan <hengjia_yan@qingsongchou.com>
2021-03-06 18:54:17 +0800

    file management

---
+++ src/com/learnfile/SeparatorAppMain.java
@@ -0,0 +1,10 @@
+package com.learnfile;
+
+import java.io.File;
+
+public class SeparatorAppMain {
+    public static void main(String[] args) {
+        System.out.println("文件路径分隔符：" + File.separator);
+        System.out.println("环境变量分隔符：" + File.pathSeparator);
+    }
+}
